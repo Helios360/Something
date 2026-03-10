524bb0d6793ed2ce4345c83ff5b3a0916d81b8ac
Nubunduram <ceantoine@hotmail.fr>
2026-03-10 12:07:14 +0100

    feat: link back & front

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
+const formParser = require('./formparser');
 const deleteOldUploads = require('./cleaner');
 const fs = require('fs').promises;
 app.disable('x-powered-by');
@@ -20,22 +21,25 @@ app.set('trust proxy', 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // === Static Files ===
-app.use(express.static(path.join('public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 // === Rate limit, anti ddos ===
 app.use(rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 200 // max 100 requests per 15 minutes
 }));
-const loginLimiter = rateLimit({windowMs: 10 * 60 * 1000, max: 30, standardHeaders: true, legacyHeaders: false});
+const loginLimiter = rateLimit({ windowMs: 10 * 60 * 1000, max: 30, standardHeaders: true, legacyHeaders: false });
 let header = "";
 let footer = "";
 // === HTML Comp Routes (Prod) ===
-async function bootstrap(){
+async function bootstrap() {
   [header, footer] = await Promise.all([
     fs.readFile(path.join('public/components/header.html'), "utf-8"),
     fs.readFile(path.join('public/components/footer.html'), "utf-8"),
   ]);
+
+  app.use(formParser);
+
   app.get('/', async (_, res) => {
     const page = await fs.readFile(path.join("public/index.html"), "utf-8");
     res.type("html").send(page.replace("<!--header-->", header).replace("<!--footer-->", footer));
@@ -44,8 +48,8 @@ async function bootstrap(){
     const page = await fs.readFile(path.join('public/register.html'), "utf-8");
     res.type("html").send(page.replace("<!--header-->", header).replace("<!--footer-->", footer));
   });
-  app.use(crudRouter);
-  app.use(testRouter);
+  // app.use(crudRouter);
+  // app.use(testRouter);
   // === Global error handler ===
   app.use((err, req, res, next) => {
     console.error('Global error handler:', err);
